10c6021a6ddea8606cb27a2935bcb8432afda712
knzee <knzee@mail.ru>
2020-04-26 21:20:41 -0700

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Test Testovich on 26/04/2020.
+//  Copyright © 2020 TSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
